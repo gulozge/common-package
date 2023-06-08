201665c8b080b52ede5dd6235a517f6ac4f046c8
gulozge <gulumozge23@gmail.com>
2023-06-08 17:52:03 +0300

    first commit

---
+++ src/main/java/com/kodlamaio/commonpackage/events/inventory/CarCreatedEvent.java
@@ -0,0 +1,26 @@
+package com.kodlamaio.commonpackage.events.inventory;
+
+import com.kodlamaio.commonpackage.events.Event;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarCreatedEvent implements Event {
+    private UUID carId;
+    private UUID modelId;
+    private UUID brandId;
+    private int modelYear;
+    private String plate;
+    private String state;
+    private double dailyPrice;
+    private String modelName;
+    private String modelBrandName;
+}
+
